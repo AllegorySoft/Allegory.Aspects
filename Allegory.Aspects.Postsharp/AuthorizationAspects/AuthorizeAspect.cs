16fa051ac24a98287cbf6c37647320369d326f77
ahmetfarukulu <ahmetfarukulu@gmail.com>
2020-12-19 19:05:28 +0300

    AuthorizeAspect added

---
+++ Allegory.Aspects.Postsharp/AuthorizationAspects/AuthorizeAspect.cs
@@ -0,0 +1,40 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Claims;
+using System.Text;
+
+namespace Allegory.Aspects.Postsharp.AuthorizationAspects
+{
+    [Serializable]
+    [MulticastAttributeUsage(MulticastTargets.Method, AllowMultiple = false, TargetExternalMemberAttributes = MulticastAttributes.Instance)]
+    public class AuthorizeAspect : OnMethodBoundaryAspect
+    {
+        public string Roles { get; set; }
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (!ClaimsPrincipal.Current.Identity.IsAuthenticated)
+                throw new SecurityException("Session not found");
+
+            string[] roles = Roles?.Split(',');
+            if (roles != null)
+            {
+                bool isAuthorized = false;
+
+                for (int i = 0; i < roles.Length; i++)
+                {
+                    if (ClaimsPrincipal.Current.IsInRole(roles[i]))
+                    {
+                        isAuthorized = true;
+                        break;
+                    }
+                }
+                if (!isAuthorized)
+                    throw new SecurityException("You are not authorized");
+            }
+            base.OnEntry(args);
+        }
+    }
+}
